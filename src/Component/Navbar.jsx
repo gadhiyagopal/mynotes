e9055a6ada024815bf33b821278e7d287107ef97
Gopal Gadhiya <gadhiyagopal0852@gmail.com>
2025-02-24 16:44:08 +0530

    add navbar

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+    <nav className="navbar bg-dark navbar-expand-lg "data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">MyNotes</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About Us</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#" >Register</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#" >Login</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#" >Add Note</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#" >View All Notes</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
